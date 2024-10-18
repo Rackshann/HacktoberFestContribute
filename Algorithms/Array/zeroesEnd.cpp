293345a92dc58dc1dbc658807b607805af830d77
Rackshann <148337296+Rackshann@users.noreply.github.com>
2024-10-19 04:58:43 +0530

    Update zeroesEnd.cpp

--- Algorithms/Array/zeroesEnd.cpp
+++ Algorithms/Array/zeroesEnd.cpp
@@ -3,42 +3,38 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     vector<int> v;
-    cout<<"insert numbers (-999 close number)"<<endl;
+    cout << "Insert numbers (-999 close number)" << endl;
     int x;
-    cin>>x;
-    while(x!= -999)
-    {
+    cin >> x;
+    
+    // Input loop
+    while (x != -999) {
         v.push_back(x);
-        cin>>x;
+        cin >> x;
     }
 
-    vector <int> zeroes;
-    vector <int> numbers;
-    for(int i=0;i<v.size();i++)
-    {
-        if(v[i]==0)
-            zeroes.push_back(v[i]);
-        else
-            numbers.push_back(v[i]);
+    vector<int> result;
+
+    // First, add non-zero elements
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] != 0) {
+            result.push_back(v[i]);
+        }
     }
 
-    v.clear();
-    for(int i=0;i<numbers.size();i++)
-    {
-        v.push_back(numbers[i]);
+    // Then, add all the zero elements
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == 0) {
+            result.push_back(v[i]);
+        }
     }
 
-    for(int i=0;i<zeroes.size();i++)
-    {
-        v.push_back(zeroes[i]);
-    }    
-    cout<<endl;
-    for(int i=0;i<v.size();i++)
-    {
-        cout<<v[i]<<" ";
+    // Print the result vector
+    cout << endl;
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
     }
 
     return 0;
