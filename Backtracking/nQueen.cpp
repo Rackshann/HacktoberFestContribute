2036804bf32f10d8dc2983637730b22a36c222a5
Rackshann <148337296+Rackshann@users.noreply.github.com>
2024-10-19 05:05:47 +0530

    Update nQueen.cpp

--- Backtracking/nQueen.cpp
+++ Backtracking/nQueen.cpp
@@ -1,82 +1,49 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-bool safequeen(int board[][10],int i,int j,int n)
-{
-    for(int row=0;row<i;row++)
-    {
-        if(board[row][j]==1)
-        {
-            return false;
-        }
+
+int main() {
+    vector<int> v;
+    cout << "Insert numbers (-999 to close the input)" << endl;
+    int x;
+    cin >> x;
+    
+    // Input loop to take numbers until -999 is entered
+    while (x != -999) {
+        v.push_back(x);
+        cin >> x;
     }
 
-    int x=i;
-    int y=j;
-    while(x>=0 && y>=0)
-    {
-        if(board[x][y]==1)
-        {
-            return false;
-        }
-        x--;
-        y--;
+    // Separate numbers and zeroes into two vectors
+    vector<int> zeroes;
+    vector<int> numbers;
+    
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == 0)
+            zeroes.push_back(v[i]);  // Collect zeroes
+        else
+            numbers.push_back(v[i]); // Collect non-zero numbers
     }
 
-    x=i;
-    y=j;
-    while(x>=0 && y<n)
-    {
-        if(board[x][y]==1)
-        {
-            return false;
-        }
-        x--;
-        y++;
+    // Clear the original vector v
+    v.clear();
+    
+    // Push all non-zero numbers first
+    for (int i = 0; i < numbers.size(); i++) {
+        v.push_back(numbers[i]);
     }
 
-    return true;
-}
-bool nqueen(int board[][10],int i,int n)
-{
-    if(i==n)
-    {
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(board[i][j]==1)
-                {
-                    cout<<"Q";
-                }
-                else
-                {
-                    cout<<"_";
-                }
-            }
-            cout<<endl;
-        }
-        cout<<endl;
-        return false;
+    // Then push all zeroes
+    for (int i = 0; i < zeroes.size(); i++) {
+        v.push_back(zeroes[i]);
     }
-    for(int j=0;j<n;j++)
-    {
-        if(safequeen(board,i,j,n))
-        {
-            board[i][j]=1;
-            if(nqueen(board,i+1,n))
-            {
-                return true;
-            }
-            board[i][j]=0;
-        }
+
+    // Output the modified vector
+    cout << endl;
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
     }
-    return false;
-}
-int main()
-{
-    int n;
-    cin>>n;
-    int board[10][10]={0};
-    nqueen(board,0,n);
+
     return 0;
 }
